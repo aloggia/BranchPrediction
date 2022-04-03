7b811a0d862d61d49f7fdbd0fb9c6448c430bb70
aloggia <aloggia22@gmail.com>
2022-04-02 21:39:05 -0400

    Program can now read in the output file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
-
-use std::env;
-use std::fs;
 use BranchPrediction::branch_predictor::predictor_pair::BranchPair;
 use BranchPrediction::branch_predictor::prediction_buffer::PredictionBuffer;
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input_file = &args[1];
-    let num_bits = &args[2];
-    let prediction_buffer_size = &args[3];
+    let input_file_string = "./".to_owned() + input_file;
+    let num_bits: &u8 = &args[2].parse::<u8>().unwrap();
+    let prediction_buffer_size: &usize = &args[3].parse::<usize>().unwrap();
+    let mut prediction_vector: Vec<prediction> = Vec::new();
 
-    let new_branch_pair = BranchPair::new(5000);
-    new_branch_pair.get_address();
+    let mut prediction_buffer = PredictionBuffer::new(*prediction_buffer_size);
+
+    if let Ok(lines) = read_lines(input_file_string) {
+        for line in lines {
+            if let Ok(prediction) = line {
+                prediction_vector.push(prediction);
+            }
+        }
+    }
 
 
+}
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
