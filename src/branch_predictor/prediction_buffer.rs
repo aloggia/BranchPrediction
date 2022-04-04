362c15450b380e2430dc74d663260ca66b3aaf66
aloggia <aloggia22@gmail.com>
2022-04-04 11:30:36 -0400

    Working on implementing function make_prediction that will make a prediction based on the prediction bit, it does NOT update the prediction bit after the prediction

--- src/branch_predictor/prediction_buffer.rs
+++ src/branch_predictor/prediction_buffer.rs
@@ -2,13 +2,15 @@ use crate::branch_predictor::predictor_pair::BranchPair;
 
 pub struct PredictionBuffer {
     buffer_size: usize,
-    buffer: Vec<BranchPair>
+    buffer: Vec<BranchPair>,
+    prediction_bit_size: u8,
 }
 
 impl PredictionBuffer {
-    pub fn new(buffer_size: usize) -> PredictionBuffer {
+    pub fn new(buffer_size: usize, prediction_bit_size: u8) -> PredictionBuffer {
         PredictionBuffer {
             buffer_size,
+            prediction_bit_size,
             buffer: Vec::with_capacity(buffer_size),
         }
     }
@@ -16,7 +18,6 @@ impl PredictionBuffer {
     pub fn get_branch_at_index(&self, index: usize) -> &BranchPair {
         &self.buffer[index]
     }
-
     pub fn get_branch_address(&self, index: usize) -> usize {
         self.buffer[index].get_address()
     }
@@ -32,5 +33,42 @@ impl PredictionBuffer {
 
     pub fn set_branch_at_index(&mut self, index: usize, in_branch: BranchPair) {
         self.buffer[index] = in_branch;
+    }
+
+    pub fn make_prediction(&mut self, addr: usize) -> bool {
+        // check if this branch instr is in our prediction buffer
+        if self.get_branch_address(addr & (self.buffer_size-1)) != addr {
+            // if this instr is not in the buffer then move it in
+            self.set_branch_at_index(addr & (self.buffer_size - 1), BranchPair::new(addr));
+        }
+        // Do variable things depending on the prediction bit size
+        match self.prediction_bit_size {
+            0 => {
+                // 0 bit predictor
+                // Always predict branch is taken
+                true
+            }
+            1 => {
+                // 1 bit predictor
+                // If the prediction bit is 0, then predict not taken
+                if self.get_branch_prediction(addr & (self.buffer_size - 1)) == 0 {
+                    false
+                } else {
+                    // otherwise predict taken
+                    true
+                }
+            }
+            2 => {
+                // 2 bit predictor
+                false
+            }
+            3 => {
+                // 3 bit predictor
+                false
+            }
+            _ => {
+                false
+            }
+        }
     }
 }
